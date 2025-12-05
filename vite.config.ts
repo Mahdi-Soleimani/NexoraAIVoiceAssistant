572f7854fc09af9bcd70e2df7d37cfb9727170fb
mahdi <mahdisoleimani2003@gmai.com>
2025-12-05 07:03:09 -0800

    Add run code github page2

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react()],
+      plugins: [],
       base: '/NexoraAIVoiceAssistant/',
       resolve: {
         alias: {
